48ca5579c3ab14d50e805003d57dfb684c1ee5dc
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-23 20:34:42 +0600

    end first 3 latest post show on the page

--- content/home.php
+++ content/home.php
@@ -1,70 +1,137 @@
-<?php 
-    // home page content 
+<?php
+// home page content 
 
 ?>
-<?php 
+<?php
 
 /**=====================================
 all post content here 
 ===================================*/
 
+
+
+// First Query: Get the latest three posts
+$latest_posts_query = new WP_Query(array(
+    'posts_per_page' => 3,
+    'orderby' => 'date',
+    'order' => 'DESC'
+));
+
+// Second Query: Get the next three posts from a specific category
+$category_posts_query = new WP_Query(array(
+    'cat' => 'your_category_id', // Replace 'your_category_id' with the actual category ID
+    'posts_per_page' => 3,
+    'orderby' => 'date',
+    'order' => 'DESC'
+));
+
 ?>
 
-  <div class="blog_container">
-          <?php
-          if (have_posts()) :
-            while (have_posts()) : the_post();
-          ?>
+<div class="blog_container">
+    <?php
 
-              <div class="blog_area">
+
+// Output latest posts
+if ($latest_posts_query->have_posts()) :
+    while ($latest_posts_query->have_posts()) : $latest_posts_query->the_post();
+?>
+        <div class="blog_area">
                 <div class="post_thumb">
-                  <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails'); ?></a>
+                    <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails'); ?></a>
                 </div>
                 <div class="post_details">
-                  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                  <?php
-                  // Get the post excerpt
-                  $post_content = get_the_content();
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php
+                    // Get the post excerpt
+                    $post_content = get_the_content();
 
-                  // Remove "[...]" from the excerpt
-                  $post_excerpt = wp_trim_words($post_content, 20, '');
+                    // Remove "[...]" from the excerpt
+                    $post_excerpt = wp_trim_words($post_content, 20, '');
 
-                  // Output the modified excerpt
-                  ?>
-                  <a href="<?php the_permalink() ?>" class="text-decoration-none text-dark">
-                    <?php echo $post_excerpt; ?>
-                  </a>
+                    // Output the modified excerpt
+                    ?>
+                    <a href="<?php the_permalink() ?>" class="text-decoration-none text-dark">
+                        <?php echo $post_excerpt; ?>
+                    </a>
 
-                <?php 
-                // Get the post time
-$post_time = get_the_time('U');
+                    <?php
+                    // Get the post time
+                    $post_time = get_the_time('U');
 
-// Get the human-readable time difference
-$human_time_diff = human_time_diff($post_time, current_time('U')) . ' ago';
+                    // Get the human-readable time difference
+                    $human_time_diff = human_time_diff($post_time, current_time('U')) . ' ago';
 
-                ?>
-                <br>
-                <span> <?php echo $human_time_diff; ?></span>
+                    ?>
+                    <br>
+                    <span> <?php echo $human_time_diff; ?></span>
                 </div>
-                
-              </div>
 
-          <?php
-            endwhile;
-          else :
-            _e('No post found');
-          endif;
-          ?>
+            </div>
+<?php
+    endwhile;
+    wp_reset_postdata();
+else :
+    // No posts found
+endif;
+// end first 3 latest post show on the page 
 
-          <div id="page_nav">
-            <?php if ('ikr_page_nave') {
-              ikr_page_nave();
-            } else {
-            ?>
-              <?php next_posts_link(); ?>
-              <?php previous_posts_link() ?>
 
-            <?php
-            } ?>
-          </div>
-        </div>
+
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+    ?>
+
+            <div class="blog_area">
+                <div class="post_thumb">
+                    <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails'); ?></a>
+                </div>
+                <div class="post_details">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php
+                    // Get the post excerpt
+                    $post_content = get_the_content();
+
+                    // Remove "[...]" from the excerpt
+                    $post_excerpt = wp_trim_words($post_content, 20, '');
+
+                    // Output the modified excerpt
+                    ?>
+                    <a href="<?php the_permalink() ?>" class="text-decoration-none text-dark">
+                        <?php echo $post_excerpt; ?>
+                    </a>
+
+                    <?php
+                    // Get the post time
+                    $post_time = get_the_time('U');
+
+                    // Get the human-readable time difference
+                    $human_time_diff = human_time_diff($post_time, current_time('U')) . ' ago';
+
+                    ?>
+                    <br>
+                    <span> <?php echo $human_time_diff; ?></span>
+                </div>
+
+            </div>
+
+    <?php
+        endwhile;
+        wp_reset_postdata();
+    else :
+        _e('No post found');
+    endif;
+    ?>
+
+    
+</div>
+<div id="page_nav">
+        <?php if ('ikr_page_nave') {
+            ikr_page_nave();
+        } else {
+        ?>
+            <?php next_posts_link(); ?>
+            <?php previous_posts_link() ?>
+
+        <?php
+        } ?>
+    </div>
